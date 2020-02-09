d1084ca26d8dc4d382b3cd8d11a3effd90eb6f59
HmimssaSoufiane <hmimssasoufiane@hotmail.com>
2020-02-10 00:12:56 +0100

    done

--- TP_Les_exceptions/src/ex2/Test.java
+++ TP_Les_exceptions/src/ex2/Test.java
@@ -12,19 +12,21 @@ public class Test {
 		} catch (ErrNat e) {
 			System.err.println("Erreur d'entier");
 		}
+		
+		// new Scope
 		try {
-			EntNat nbr;
-			EntNat nbr2;
-			nbr = new EntNat(1548);
-			nbr2 = new EntNat(12548);
+			EntNat number1;
+			EntNat number2;
+			number1 = new EntNat(1548);
+			number2 = new EntNat(12548);
 			EntNat res;
-			System.out.println("Nombre 1= " + nbr.getN());
-			System.out.println("Nombre 2= " + nbr2.getN());
-			res = EntNat.somme(nbr, nbr2);
+			System.out.println("Nombre 1= " + number1.getN());
+			System.out.println("Nombre 2= " + number2.getN());
+			res = EntNat.somme(number1, number2);
 			System.out.println("La somme = " + res.getN());
-			res = EntNat.diff(nbr, nbr2);
+			res = EntNat.diff(number1, number2);
 			System.out.println("La diff = " + res.getN());
-			res = EntNat.produit(nbr, nbr2);
+			res = EntNat.produit(number1, number2);
 			System.out.println("Le produit = " + res.getN());
 		} catch (ErrConst e) {
 			System.err.println("Erreur de construction du nombre" + e.getNum());
